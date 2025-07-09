4c3b8ea062985d2ea2a1fc39503166249ba673c3
Lars Schiller <derlarsschneider@web.de>
2025-07-09 10:33:46 +0200

    fix scripts

---
+++ build-scripts/release-apk.sh
@@ -0,0 +1,2 @@
+version="$(date +%y.%m.%d.%H.%M)"
+firebase appdistribution:distribute build/app/outputs/flutter-apk/app-release.apk --app 1:336568095877:android:f757f959bbe6c96be8c5ec --release-notes "Version ${version}"
