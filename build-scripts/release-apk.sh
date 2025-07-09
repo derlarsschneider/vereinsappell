a991707b9fc24577cc29f35f0870c3090eecc95b
Lars Schiller <derlarsschneider@web.de>
2025-07-09 12:00:01 +0200

    Add tester group to release

--- build-scripts/release-apk.sh
+++ build-scripts/release-apk.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 version="$(date +%y.%m.%d.%H.%M)"
-firebase appdistribution:distribute build/app/outputs/flutter-apk/app-release.apk --app 1:336568095877:android:f757f959bbe6c96be8c5ec --release-notes "Version ${version}"
+firebase appdistribution:distribute build/app/outputs/flutter-apk/app-release.apk \
+  --app 1:336568095877:android:f757f959bbe6c96be8c5ec \
+  --release-notes "Version ${version}" \
+  --groups "Schuetzenlust"
