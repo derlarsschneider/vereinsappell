91f488847315833b76ace71ecd2b15c888ea89bd
Lars Schiller <derlarsschneider@web.de>
2025-08-12 18:49:32 +0200

    improve backend skript

--- aws_backend/update_backend.sh
+++ aws_backend/update_backend.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
+# parse command line parameters
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --skip-build)
+      SKIP_BUILD="1"
+      shift
+      ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1
+      ;;
+  esac
+done
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
@@ -9,6 +23,13 @@ lambda/zip.sh
 terraform apply -auto-approve
 
 authorizer/update.sh
-lambda/build.sh && lambda/update.sh
+
+# if flag --skip-build is set, don't build the lambda
+if [ "${SKIP_BUILD}" == "1" ]; then
+  echo "⚠️ --skip-build flag set, skipping lambda build."
+  lambda/update.sh
+else
+  lambda/build.sh && lambda/update.sh
+fi
 
 echo "✅ Backend wurde aktualisiert."
