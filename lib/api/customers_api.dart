bce7e4fe8800515aebf77ac87ed3156911361bdf
Lars Schiller <derlarsschneider@web.de>
2026-04-16 11:25:06 +0200

    feat: add listCustomers, updateCustomer, createCustomer to CustomersApi

--- lib/api/customers_api.dart
+++ lib/api/customers_api.dart
@@ -23,4 +23,41 @@ class CustomersApi {
       throw Exception('Fehler beim Laden des Vereins: ${response.statusCode}');
     }
   }
+
+  Future<List<Map<String, dynamic>>> listCustomers() async {
+    final response = await _client.get(
+      Uri.parse('${config.apiBaseUrl}/customers'),
+      headers: headers(config),
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.cast<Map<String, dynamic>>();
+    } else {
+      throw Exception('Fehler beim Laden der Vereine: ${response.statusCode}');
+    }
+  }
+
+  Future<void> updateCustomer(String id, Map<String, dynamic> data) async {
+    final response = await _client.put(
+      Uri.parse('${config.apiBaseUrl}/customers/$id'),
+      headers: headers(config),
+      body: json.encode(data),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Fehler beim Speichern: ${response.statusCode}');
+    }
+  }
+
+  Future<Map<String, dynamic>> createCustomer(Map<String, dynamic> data) async {
+    final response = await _client.post(
+      Uri.parse('${config.apiBaseUrl}/customers'),
+      headers: headers(config),
+      body: json.encode(data),
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Fehler beim Erstellen: ${response.statusCode}');
+    }
+  }
 }
