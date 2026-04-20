e380a1cbc6818da9d9c89af342d9652ece111d00
Lars Schiller <derlarsschneider@web.de>
2026-04-20 10:47:09 +0200

    fix: version number tz

--- build-scripts/build-web.sh
+++ build-scripts/build-web.sh
@@ -6,16 +6,16 @@ if [ "$#" -gt 0 ]; then
   BUILD_NAME="$1"
   shift
 else
-  BUILD_NAME=$(date +"%Y.%m.%d")
+  BUILD_NAME=$(TZ=Europe/Berlin date +"%Y.%m.%d")
 fi
 if [ "$#" -gt 0 ]; then
   BUILD_NUMBER="$1"
   shift
 else
-  BUILD_NUMBER=$(date +"%H%M")
+  BUILD_NUMBER=$(TZ=Europe/Berlin date +"%H%M")
 fi
 
-"$SCRIPT_DIR/bump-version.sh" "${BUILD_NAME}.${BUILD_NUMBER}"
+"$SCRIPT_DIR/bump-version.sh" "${BUILD_NAME}+${BUILD_NUMBER}"
 echo "📦 Baue Web App mit Version ${BUILD_NAME}+${BUILD_NUMBER}"
 
 flutter build web --build-name="${BUILD_NAME}" --build-number="${BUILD_NUMBER}" --release
