35164b8c5f7130b8ba2ed48c65a90366890a9ff8
Lars Schiller <derlarsschneider@web.de>
2026-04-22 08:49:41 +0200

    fix: use correct source ARN for Lambda authorizer permission

--- aws_backend/lambda_authorizer.tf
+++ aws_backend/lambda_authorizer.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_permission" "auth_api_permission" {
     action        = "lambda:InvokeFunction"
     function_name = aws_lambda_function.lambda_authorizer.function_name
     principal     = "apigateway.amazonaws.com"
-    source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+    source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/authorizers/${aws_apigatewayv2_authorizer.lambda_authorizer.id}"
 }
 
 # Lambda Authorizer
